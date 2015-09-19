493d0dc55b71c532ccdd138650d80958064f5cf7
Ivan Kalita <input.txt@hotmail.com>
2015-09-19 22:56:42 +1000

    Remove sudo

--- scripts/install-googletest.sh
+++ scripts/install-googletest.sh
@@ -2,6 +2,6 @@
 wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
 unzip gtest-1.7.0.zip
 cd gtest-1.7.0 && ./configure && make
-sudo cp -a lib/.libs/* /usr/lib/
+cp -a lib/.libs/* /usr/local/lib/
 cd ..
 rm -rf gtest-1.7.0*

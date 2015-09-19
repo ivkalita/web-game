fdfb22e34e6b4d232feff1a815229c9e22716ede
Ivan Kalita <input.txt@hotmail.com>
2015-09-19 23:20:50 +1000

    Still trying to fix travis system

--- scripts/install-googletest.sh
+++ scripts/install-googletest.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
 unzip gtest-1.7.0.zip
+export LD_LIBRARY_PATH=/usr/lib/
 cd gtest-1.7.0 && ./configure && make
-sudo cp -a lib/.libs/* /usr/local/lib/
+sudo cp -a lib/.libs/* /usr/lib/
 cd ..
 rm -rf gtest-1.7.0*

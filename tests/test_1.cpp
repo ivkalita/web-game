cbf9d46828a77b6cab23fdae00592fbb8cbccaab
Ivan Kalita <input.txt@hotmail.com>
2015-09-22 15:20:54 +1000

    Make example tests green

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 TEST(Example1, Equal) {
-    ASSERT_EQ(1, 2) << "1 not equal 2";
+    ASSERT_EQ(1, 1) << "1 equal 1";
 }
 
 TEST(Example1, ReallyEqual) {
--- tests/test_2.cpp
+++ tests/test_2.cpp
@@ -1,9 +1,9 @@
 #include <gtest/gtest.h>
 
 TEST(Example2, NotEqual) {
-    ASSERT_NE(1, 1) << "1 equal 1";
+    ASSERT_NEQ(1, 2) << "1 not equal 2";
 }
 
 TEST(Example2, ReallyNotEqual) {
-    ASSERT_NE(1, 2) << "1 not equal 2";
+    ASSERT_NEQ(1, 2) << "1 not equal 2";
 }

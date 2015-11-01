5f5c01c4e68efc126295e38178fb8c20bc9115d5
mariner1 <aledapin@yandex.ru>
2015-11-02 01:43:22 +1000

    Add tests configuration file Now configuration for tests is loaded from tester.ini. tester.ini is loaded with IniFileConfiguration() and set explicitly. loadConfiguration() is more flexible, but we can't use loadConfiguration() for DBConnector unit testing without working instance of server application.

--- tests/DBConnector/test_1.cpp
+++ tests/DBConnector/test_1.cpp
@@ -1,45 +1,65 @@
 #include "DBConnector.hpp"
+#include "WebgameServer.hpp"
+#include "Poco/Util/IniFileConfiguration.h"
 #include <gtest/gtest.h>
+
+using Poco::AutoPtr;
+using Poco::Util::IniFileConfiguration;
+
+class QueryTest : public ::testing::Test {
+public:
+    AutoPtr<IniFileConfiguration> c;
+
+    virtual void SetUp() {
+        //WebgameServer::instance().loadConfiguration();
+        //auto c = &WebgameServer::instance().config();
+        c = new IniFileConfiguration("bin/tester.ini");
+      
+        DBConnection::instance().Connect(
+            c->getString("tester.hostaddr"),
+            c->getString("tester.port"),
+            c->getString("tester.dbname"),
+            c->getString("tester.user"),
+            c->getString("tester.password"));
+    }
+
+    virtual void TearDown() {
+        DBConnection::instance().Disconnect();
+    }
+};
+
 
 TEST(SingletonTest, Equal) {
     ASSERT_EQ(&DBConnection::instance(), &DBConnection::instance()) << "instance is the same address";
 }
 
-const char* password = "qwe"; // specify your password
-const char* dbname = "galcon"; // specify your database name
 
-TEST(QueryTest, SelectInserted) {
-    DBConnection inst = DBConnection::instance();
-    inst.Connect("127.0.0.1", "5432", dbname, "postgres", password);
+TEST_F(QueryTest, SelectInserted) {
+   
     char* pl_name = "test'player";
-    inst.ExecParams("DELETE FROM players where login=$1", { pl_name });
-    inst.ExecParams("INSERT INTO players (login, password) values ($1, $2)", { pl_name, "pswd" });
-    auto res = inst.ExecParams("SELECT * FROM players where login=$1", { pl_name });
+    DBConnection::instance().ExecParams("DELETE FROM players where login=$1", { pl_name });
+    DBConnection::instance().ExecParams("INSERT INTO players (login, password) values ($1, $2)", { pl_name, "pswd" });
+    auto res = DBConnection::instance().ExecParams("SELECT * FROM players where login=$1", { pl_name });
     for (auto i = res.begin(); i != res.end(); ++i) {
         ASSERT_EQ((*i).field_by_name("login"), string(pl_name));
     }
-    inst.Disconnect();
 }
 
-TEST(QueryTest, PreparedStmt) {
-    DBConnection inst = DBConnection::instance();
-    inst.Connect("127.0.0.1", "5432", dbname, "postgres", password);
-
+TEST_F(QueryTest, PreparedStmt) {
     char* names[3][2] = { { "login1", "pwd1" }, { "login2", "pwd2" }, { "login3", "pwd3" } };
 
-    inst.ExecParams("DELETE FROM players", {});
+    DBConnection::instance().ExecParams("DELETE FROM players", {});
 
-    auto ps = inst.CreatePreparedStatement("INSERT INTO players (login, password) values ($1, $2)");
+    auto ps = DBConnection::instance().Prepare("INSERT INTO players (login, password) values ($1, $2)");
 
     for (int i = 0; i < 3; i++) {
         ps.Exec({ names[i][0], names[i][1] });
     }
 
-    auto res = inst.ExecParams("SELECT * FROM players ORDER BY login", {});
+    auto res = DBConnection::instance().ExecParams("SELECT * FROM players ORDER BY login", {});
     int j = 0;
     for (auto i = res.begin(); i != res.end(); ++i) {
-        ASSERT_EQ((*i).field_by_name("login"), string(names[j++][0]));
+        EXPECT_EQ((*i).field_by_name("login"), string(names[j++][0]));
     }
-    inst.Disconnect();
 
 }

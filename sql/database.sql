ede360369df0b5690685fa8ce8826cffc2f8adc2
Shutov Roman <Shifouse@gmail.com>
2015-12-10 15:57:11 +1000

    Edited database

--- sql/database.sql
+++ sql/database.sql
@@ -6,13 +6,6 @@ GRANT ALL privileges ON DATABASE "web-game" TO "web-game";
 
 \c web-game web-game
 
-CREATE SEQUENCE players_id_seq
-    INCREMENT 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1;
-
 CREATE SEQUENCE games_id_seq
     INCREMENT 1
     MINVALUE 1
@@ -30,17 +23,6 @@ CREATE SEQUENCE connections_id_seq
     START 1
     CACHE 1;
 
-CREATE TABLE players
-(
-    id integer NOT NULL DEFAULT nextval('players_id_seq'::regclass),
-    login character varying(30) UNIQUE,
-    password character varying(30),
-    CONSTRAINT players_pkey PRIMARY KEY (id)
-)
-WITH (
-    OIDS=FALSE
-);
-
 
 CREATE TABLE games
 (
@@ -48,7 +30,7 @@ CREATE TABLE games
     name character varying(50),
     maxNumPlayers integer NOT NULL,
     curNumPlayers integer NOT NULL,
-    owner_id integer references players(id) UNIQUE,
+    owner_id integer references users(id) UNIQUE,
     CHECK (curNumPlayers <= maxNumPlayers),
     CONSTRAINT games_pkey PRIMARY KEY (id)
 )
@@ -60,7 +42,7 @@ CREATE TABLE connections
 (
     id integer NOT NULL DEFAULT nextval('connections_id_seq'::regclass),
     game_id integer references games(id) ON DELETE CASCADE,
-    player_id integer references players(id) UNIQUE,
+    player_id integer references users(id) UNIQUE,
     CONSTRAINT connections_pkey PRIMARY KEY (id)
 )
 WITH (

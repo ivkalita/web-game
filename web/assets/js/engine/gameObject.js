6fd16ab51dc08d6cbe420f141018e4aea9ffcf5b
Denis <denis_davidyuk@hotmail.com>
2015-12-27 17:52:21 +1000

    Sawing of the engine

--- web/assets/js/engine/gameObject.js
+++ web/assets/js/engine/gameObject.js
@@ -1,121 +1,79 @@
-//class GameObject
-function GameObject(x, y, vertices, vertSize, vertCount, attribs, uniforms, boundingBox) {
+function compileShader(gl, code, type) { //type: gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
+    var shader = shader = gl.createShader(type);
+    gl.shaderSource(shader, code);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(shader));
+        return null;
+    }
+    return shader;
+}
+
+function linkShaderProgram(gl, shaders) {
+    var program = gl.createProgram();
+    for (var i = 0; i < shaders.length; i++) {
+        gl.attachShader(program, shaders[i]);
+    }
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        alert("Could not initialize shader program");
+    }
+    return program;
+}
+
+function Ship(x, y, angle) {
+    var _scale = 0.1;
     this._x = x;
     this._y = y;
-    var _vertices = vertices;
-    var _vertSize = vertSize;
-    var _vertCount = vertCount;
-    var _attribs = attribs;
-    var _uniforms = uniforms;
-    var _boundingBox = boundingBox;
-
-    Object.defineProperties(this, {
-        "vertices": { value: _vertices, writable: true },
-        "vertSize": { value: _vertSize, writable: true },
-        "vertCount": { value: _vertCount, writable: true },
-        "attribs": { value: _attribs, writable: true },
-        "uniforms": { value: _uniforms, writable: true },
-        "boundingBox": { value: _boundingBox, writable: true }
-    });
-}
-
-GameObject.prototype.constructor = GameObject;
-
-//class Ship
-function Ship(x, y, speedVector) {
-    var _board = 0.1;
-
-    this.calcShipVertices = function (x, y, l) {
-        return [
-            x - l / 2, y - Math.sqrt(3) / 6 * l, 0.0,
-            x,         y + Math.sqrt(3) / 3 * l, 0.0,
-            x + l / 2, y - Math.sqrt(3) / 6 * l, 0.0
-        ];
-    };
-
-    GameObject.apply(this, [
-        x, y, this.calcShipVertices(x, y, _board), 3, 3, {"aVertexPosition": 0}, {"uPMatrix": [], "uMVMatrix": []},
-        null]);
-
-    this.calcDirection = function(v) {
-        if (v[0] == 0) return 0;
-        var rad = Math.acos(v[0] / Math.sqrt(Math.pow(v[0], 2) + Math.pow(v[1], 2)));
-        return 180.0 / Math.PI * rad;
-    };
-
-    var _direction = this.calcDirection(speedVector);
-
-    Object.defineProperties(this, {
-        "x": {
-            get: function() { return this._x; },
-            set: function(newX) {
-                this._x = newX; this.vertices = this.calcShipVertices(newX, y, _board);
-            }},
-        "y": {
-            get: function() { return this._y; },
-            set: function(newY) {
-                this._y = newY; this.vertices = this.calcShipVertices(x, newY, _board);
-            }},
-        "board": { value: _board, writable: true },
-        "direction": { value: _direction, writable: true  }
-    });
-}
-
-Ship.prototype = Object.create(GameObject.prototype);
-Ship.prototype.constructor = Ship;
-
-//class Planet
-function Planet(x, y) {
-    var _radius = 0.5;
-    var _shipsCount = 0;
-
-    this.calcPlanetVertices = function(x, y, r) {
-        var vertices = [
-            x,     y, 0.0,
-            x + r, y, 0.0
-        ];
-        var polyNum = 100;
-        var degPerPoly = (2 * Math.PI) / polyNum;
-
-        for (var i = 0; i <= polyNum; i++) {
-            var index = 2 * 3 + i * 3;
-            var angle = degPerPoly * (i + 1);
-
-            vertices[index] = x + r * Math.cos(angle);
-            vertices[index + 1] = y + r * Math.sin(angle);
-            vertices[index + 2] = 0;
+    this._angle = angle;
+    this._transMat = [];
+    this._verticles = [];
+    this._vertexShader = '\
+        attribute vec2 aVertexPosition;\
+        uniform mat4 uMVMatrix;\
+        uniform mat4 uPMatrix;\
+        void main(void) {\
+            gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 0.0, 1.0);\
+        }';
+    this._fragmentShader = '\
+        void main(void) {\
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\
+        }';
+    this.program = null;
+    
+    this.draw = function(gl, perspectMat){
+        if (!this._program) {
+            var shaders = [];
+            shaders.push(compileShader(gl, this._vertexShader, gl.VERTEX_SHADER));
+            shaders.push(compileShader(gl, this._fragmentShader, gl.FRAGMENT_SHADER));
+            this._program = linkShaderProgram(gl, shaders);
         }
-
-        return vertices;
+        gl.useProgram(this._program);
+        
+        var _vertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, _vertexPositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this._verticles), gl.STATIC_DRAW);
+        var attribLocation = gl.getAttribLocation(this._program, 'aVertexPosition');
+        gl.bindBuffer(gl.ARRAY_BUFFER, _vertexPositionBuffer);
+        gl.enableVertexAttribArray(attribLocation);
+        gl.vertexAttribPointer(attribLocation, 2, gl.FLOAT, false, 0, 0);
+        
+        gl.uniformMatrix4fv(gl.getUniformLocation(this._program, 'uPMatrix'), false, perspectMat);
+        gl.uniformMatrix4fv(gl.getUniformLocation(this._program, 'uMVMatrix'), false, this._transMat);
+        
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, this._verticles.length / 2);
     };
-
-    this.calcBoundingBox = function(x, y, r) {
-        return {"x": x - r, "y": y + r, "width": 2 * r, "height": 2 * r };
+    
+    this.updateVertices = function () {
+        this._verticles = [];
+        this._verticles = this._verticles.concat([0, -0.5]);
+        this._verticles = this._verticles.concat([-1, -1]);
+        this._verticles = this._verticles.concat([0, 1.5]);
+        this._verticles = this._verticles.concat([1, -1]);
+        mat4.identity(this._transMat);
+        mat4.translate(this._transMat, this._transMat, [this._x, this._y, -1.0]);
+        mat4.rotate(this._transMat, this._transMat, this._angle, [0, 0, 1]);
+        mat4.scale(this._transMat, this._transMat, [_scale, _scale, _scale])
     };
-
-    GameObject.apply(this, [
-        x, y, this.calcPlanetVertices(x, y, _radius), 3, 103, {"aVertexPosition": 0, "aColor": [1.0, 1.0, 1.0, 1.0]},
-        {"uPMatrix": [], "uMVMatrix": []}, this.calcBoundingBox(x, y, _radius)]);
-
-    Object.defineProperties(this, {
-        "x": {
-            get: function() { return this._x; },
-            set: function(newX) {
-                this._x = newX; this.vertices = this.calcPlanetVertices(newX, y, _radius);
-            }},
-        "y": {
-            get: function() { return this._y; },
-            set: function(newY) {
-                this._y = newY; this.vertices = this.calcPlanetVertices(x, newY, _radius);
-            }},
-        "radius": {
-            get: function() { return _radius; },
-            set: function(radius) {
-                _radius = radius; this.vertices = this.calcPlanetVertices(x, y, radius);
-            }},
-        "shipsCount": { value: _shipsCount, writable: true }
-    });
+    this.updateVertices();
 }
-
-Planet.prototype = Object.create(GameObject.prototype);
-Planet.prototype.constructor = Planet;
--- web/assets/js/engine/scene.js
+++ web/assets/js/engine/scene.js
@@ -1,57 +1,75 @@
 function Scene(canvas) {
-    var _canvas = canvas;
-    var _gameObjects = {};
-    var _needUpdate = true;
-
+    this._canvas = canvas;
+    this._gameObjects = [];
+    this._needUpdate = true;
+    this._gl;
+    
     this.render = function() {
-        new Engine(this).render();
+        if (this._needUpdate) this.draw();
     };
-
-    this.updateObjects = function() { updateObjects(this); };
+    
+    this.draw = function() {
+        this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
+        //this._gl.viewport(0, 0, this._gl.viewportHeight, this._gl.viewportWidth);
+        var perspectMat = [];
+        mat4.perspective(perspectMat, 90, this._gl.viewportWidth / this._gl.viewportHeight, 0.1, 100.0);
+        for (var i = 0; i < this._gameObjects.length; i++) {
+            this._gameObjects[i].draw(this._gl, perspectMat);
+        }
+        this._needUpdate = false;
+    };
 
     this.addObject = function(object) {
-        var coordsKey = String(object.x) + ":" + String(object.y);
-        _gameObjects[coordsKey] = object;
+        this._gameObjects.push(object);
     };
-
-    this.removeObjectAtCoords = function(coords) { //bad function, it needs to think how to kill objects properly
-        var coordsKey = String(coords[0]) + ":" + String(coords[1]);
-        delete _gameObjects[coordsKey];
-    };
+    
+    this.invalidate = function() {
+        this._needUpdate = true;
+    }
+    
+    this.initGl();
 
     Object.defineProperties(this, {
-        "gameObjects": { value: _gameObjects, writable: true },
-        "canvas": { value: _canvas, writable: true },
-        "needUpdate": { value: _needUpdate, writable: true }
     });
 }
 
-Scene.prototype.constructor = Scene;
+Scene.prototype.initGl = function() {
+    var width = this._canvas.clientWidth;
+    var height = this._canvas.clientHeight;
+    if (this._canvas.width != width || this._canvas.height != height) {
+        this._canvas.width = width;
+        this._canvas.height = height;
+    }
+    this._gl = null;
+    var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+    for (var i = 0; i < names.length; i++) {
+        try {
+            this._gl = this._canvas.getContext(names[i]);
+        } catch(e) {}
 
-function startGame() {
-    s = new Scene($("#game-screen")[0]);
-    s.render();
+        if (this._gl) {
+            this._gl.viewportWidth = this._canvas.width;
+            this._gl.viewportHeight = this._canvas.height;
+
+            break;
+        } else {
+            alert("Could not initialize WebGL");
+        }
+    }
+    this._gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this._gl.enable(this._gl.DEPTH_TEST);
+    requestAnimationFrame(this.render.bind(this));
+    this.draw();
 }
 
-function updateObjects(scene) {
-    //the code to receive messages from server needs to be here
-
-    if (scene.needUpdate) {
-        //some testing code here
-        s = new Ship(0.0, 0.0, [3.0, 1.0]);
-        s.x = 1.0;
-        alert(s.x);
-        scene.addObject(s);
-
-        p = new Planet(2.0, 0.0);
-        p.radius = 1;
-        scene.addObject(p);
-        scene.addObject(new Planet(1.0, 1.0));
-
-        scene.addObject(new Ship(0.0, 1.0, [1.0, 1.0]));
-
-        scene.removeObjectAtCoords([1.0, 1.0]);
-    }
-
-    scene.needUpdate = false;
+function startGame() {
+    scene = new Scene($("#game-screen")[0]);
+    
+    s = new Ship(0.0, 0.0, 0);
+    s.x = 1.0;
+    scene.addObject(s);
+    scene.addObject(new Ship(0.0, 1.0, 0));
+    
+    scene.invalidate();
+    scene.render();
 }

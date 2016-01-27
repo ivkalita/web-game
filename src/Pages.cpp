6d98a36fb4ae76de5414e5d8e2f8b341efc32ddf
Denis <denis_davidyuk@hotmail.com>
2016-01-27 15:24:31 +1000

    Add unnamed namespace, remove static keyword

--- src/Pages.cpp
+++ src/Pages.cpp
@@ -5,93 +5,95 @@
 #include "Poco/JSON/Template.h"
 #include "Poco/JSON/Object.h"
 
-using Poco::Util::Application;
-
-static void root(const RouteMatch& m) {
-    Poco::JSON::Template tpl(Application::instance().config().getString("application.rootpath") + "views/index.html");
-    tpl.parse();
-    std::ostream& st = m.response().send();
-    tpl.render(Poco::JSON::Object(), st);
-    st.flush();
-}
-
-static void http_example(const RouteMatch& m) {
-    auto & s = m.response().send();
-    s << "Hello world!";
-    auto & db = DBConnection::instance();
-    auto res = db.ExecParams("SELECT * FROM users", {});
-    for (auto it = res.begin(); it != res.end(); ++it) {
-        s << std::endl << (*it).field_by_name("name") << std::endl;
-    }
-}
-
 #include "Poco/Net/WebSocket.h"
 #include "Poco/Net/NetException.h"
 
-static void websocket_example(const RouteMatch& m) {
-    auto & app = WebgameServer::instance();
+namespace {
+    using Poco::Util::Application;
 
-    try {
-        Poco::Net::WebSocket ws(m.request(), m.response());
-        app.logger().information("WebSocket connection established.");
-        char buffer[1024];
-        int flags;
-        int n;
-
-        do {
-            n = ws.receiveFrame(buffer, sizeof(buffer), flags);
-            app.logger().information(Poco::format("Frame received (length=%d, flags=0x%x).", n, unsigned(flags)));
-            ws.sendFrame(buffer, n, flags);
-        } while (n > 0 || (flags & Poco::Net::WebSocket::FRAME_OP_BITMASK) != Poco::Net::WebSocket::FRAME_OP_CLOSE);
-
-        app.logger().information("WebSocket connection closed.");
+    void root(const RouteMatch& m) {
+        Poco::JSON::Template tpl(Application::instance().config().getString("application.rootpath") + "views/index.html");
+        tpl.parse();
+        std::ostream& st = m.response().send();
+        tpl.render(Poco::JSON::Object(), st);
+        st.flush();
     }
 
-    catch (Poco::Net::WebSocketException& exc) {
-        app.logger().log(exc);
-
-        switch (exc.code()) {
-        case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
-            m.response().set("Sec-WebSocket-Version", Poco::Net::WebSocket::WEBSOCKET_VERSION);
-            // fallthrough
-        case Poco::Net::WebSocket::WS_ERR_NO_HANDSHAKE:
-        case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
-        case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_NO_KEY:
-            m.response().setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
-            m.response().setContentLength(0);
-            m.response().send();
-            break;
+    void http_example(const RouteMatch& m) {
+        auto & s = m.response().send();
+        s << "Hello world!";
+        auto & db = DBConnection::instance();
+        auto res = db.ExecParams("SELECT * FROM users", {});
+        for (auto it = res.begin(); it != res.end(); ++it) {
+            s << std::endl << (*it).field_by_name("name") << std::endl;
         }
     }
-}
 
-static void game(const RouteMatch& m) {
-    m.response().redirect("/game.html");
-}
+    void websocket_example(const RouteMatch& m) {
+        auto & app = WebgameServer::instance();
 
-static void games(const RouteMatch& m) {
-    Poco::JSON::Template tpl(Application::instance().config().getString("application.rootpath") + "views/games.html");
-    tpl.parse();
-    std::ostream& st = m.response().send();
+        try {
+            Poco::Net::WebSocket ws(m.request(), m.response());
+            app.logger().information("WebSocket connection established.");
+            char buffer[1024];
+            int flags;
+            int n;
 
-    Poco::JSON::Object params = Poco::JSON::Object();
-    params.set("title", "All games");
-    params.set("isAuthorized", true);
+            do {
+                n = ws.receiveFrame(buffer, sizeof(buffer), flags);
+                app.logger().information(Poco::format("Frame received (length=%d, flags=0x%x).", n, unsigned(flags)));
+                ws.sendFrame(buffer, n, flags);
+            } while (n > 0 || (flags & Poco::Net::WebSocket::FRAME_OP_BITMASK) != Poco::Net::WebSocket::FRAME_OP_CLOSE);
 
-    tpl.render(params, st);
-    st.flush();
-}
+            app.logger().information("WebSocket connection closed.");
+        }
 
-class Pages {
-public:
-    Pages() {
-        auto & router = Router::instance();
-        router.registerRoute("/", root);
-        router.registerRoute("/hw", http_example);
-        router.registerRoute("/ws", websocket_example);
-        router.registerRoute("/game", game);
-        router.registerRoute("/games", games);
+        catch (Poco::Net::WebSocketException& exc) {
+            app.logger().log(exc);
+
+            switch (exc.code()) {
+            case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
+                m.response().set("Sec-WebSocket-Version", Poco::Net::WebSocket::WEBSOCKET_VERSION);
+                // fallthrough
+            case Poco::Net::WebSocket::WS_ERR_NO_HANDSHAKE:
+            case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
+            case Poco::Net::WebSocket::WS_ERR_HANDSHAKE_NO_KEY:
+                m.response().setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
+                m.response().setContentLength(0);
+                m.response().send();
+                break;
+            }
+        }
     }
-};
 
-static Pages pages;
+    void game(const RouteMatch& m) {
+        m.response().redirect("/game.html");
+    }
+
+    void games(const RouteMatch& m) {
+        Poco::JSON::Template tpl(Application::instance().config().getString("application.rootpath") + "views/games.html");
+        tpl.parse();
+        std::ostream& st = m.response().send();
+
+        Poco::JSON::Object params = Poco::JSON::Object();
+        params.set("title", "All games");
+        params.set("isAuthorized", true);
+
+        tpl.render(params, st);
+        st.flush();
+    }
+
+    class Pages {
+    public:
+        Pages() {
+            auto & router = Router::instance();
+            router.registerRoute("/", root);
+            router.registerRoute("/hw", http_example);
+            router.registerRoute("/ws", websocket_example);
+            router.registerRoute("/game", game);
+            router.registerRoute("/games", games);
+        }
+    };
+
+    Pages pages;
+}

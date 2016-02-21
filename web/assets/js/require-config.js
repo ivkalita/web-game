248e4a595434859ced2e8d260f456f2cc9241c32
Denis <denis_davidyuk@hotmail.com>
2016-02-22 01:50:29 +1000

    Add config for require.js

---
+++ web/assets/js/require-config.js
@@ -0,0 +1,13 @@
+requirejs.config({
+    baseUrl: "assets/js",
+    paths: {
+        jquery: 'jquery-2.1.4.min'
+    },
+    packages: [
+        {
+            name: 'pixi',
+            location: 'pixi',
+            main: 'pixi.min'
+        }
+    ]
+});

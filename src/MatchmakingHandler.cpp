504c3467aecabd2ab1bbda6edbd72956b64806e1
Shutov Roman <Shifouse@gmail.com>
2016-03-27 12:49:05 +1000

    added matchmaking test page
    Based on @RShutov 713a78eae9f72d62bfc371482cfa2c18244a76d6

---
+++ src/MatchmakingHandler.cpp
@@ -0,0 +1,34 @@
+#include "Router.hpp"
+#include "User.hpp"
+#include "Matchmaking.hpp"
+
+#include "Poco/JSON/Template.h"
+#include "Poco/JSON/Object.h"
+
+namespace {
+    static void lobby(const RouteMatch& m) {
+        auto params = m.captures();
+        std::string accessToken = params.at("accessToken");
+        User user(accessToken);
+        Matchmaking::CreateConnection(user, Poco::Net::WebSocket(m.request(), m.response()));
+    }
+
+    static void getLobby(const RouteMatch& m) {
+        Poco::JSON::Template tpl("views/matchmaking.html");
+        tpl.parse();
+        std::ostream& st = m.response().send();
+        tpl.render(Poco::JSON::Object(), st);
+        st.flush();
+    }
+
+    class Pages {
+    public:
+        Pages() {
+            auto & router = Router::instance();
+            router.registerRoute("/getLobby", getLobby);
+            router.registerRoute("/lobby/{accessToken}", lobby);
+        }
+    };
+
+    static Pages pages;
+}

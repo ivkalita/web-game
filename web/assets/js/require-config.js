56ca1890e656c74bd37492fb9c000c2744d4ace0
Denis <denis_davidyuk@hotmail.com>
2016-03-30 11:31:48 +1000

    Update require-config

--- web/assets/js/require-config.js
+++ web/assets/js/require-config.js
@@ -1,8 +1,27 @@
 requirejs.config({
     baseUrl: "assets/js",
     paths: {
+        bootstrap: 'bootstrap.min',
+        backbone: 'backbone-min',
+        underscore: 'underscore-min',
         jquery: 'jquery-2.1.4.min'
     },
+
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    },
+
     packages: [
         {
             name: 'pixi',

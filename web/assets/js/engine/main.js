77172fb4bea53c002dfb4d9f63a9a1a2439009db
Shutov Roman <Shifouse@gmail.com>
2016-04-13 12:13:03 +1000

    edited client Engine

--- web/assets/js/engine/main.js
+++ web/assets/js/engine/main.js
@@ -1,8 +1,26 @@
 define(
     ['pixi', './objects/planet', './objects/ship', './selection', './tools', './background', 'pixi/pixiTouchOverOutPatch'],
     function(PIXI, Planet, Ship, Selection, Tools, Background) {
-        function Engine(width, height, sendShipsHandler, noWebGL) {
-            this._renderer = PIXI.autoDetectRenderer(width, height, { antialias: true }, noWebGL);
+
+
+        function Engine(sendShipsHandler, noWebGL) {
+            this._width = window.innerWidth;
+            this._height = window.innerHeigh;
+
+            this._renderer = PIXI.autoDetectRenderer(this._width,  this._height, { antialias: true }, noWebGL);
+            this._width;
+            this._height;
+            this.colorScheme = [0xFe4C00, 0xFFD700, 0x2A52BE];
+            this.ratio;
+            this.xshift = 0;
+            this.yshift = 0;
+            this.scaleheight;
+            this.scalewidth;
+            this.scalerad;
+            this.scaleShip;
+            this.shipCountAtSide = 40;
+
+
             this.view = this._renderer.view;
             this._sendShipsHandler = sendShipsHandler;
             this._sendShipsPercent = 0.5;
@@ -12,13 +30,20 @@ define(
             this._ships = [];
 
             this._stage = new PIXI.Container();
-            this._background = new Background(width, height);
-            this._stage.addChild(this._background);
 
+            aHeight = window.screen.availHeight
+            aWidth = window.screen.availWidth
+
+            this._background = new Background(aWidth, aHeight);
+            this._stage.addChild(this._background);
             this._selectEventLayer = new PIXI.Graphics();
             this._selectEventLayer.beginFill(0x000000, 0);
-            this._selectEventLayer.drawRect(0, 0, width, height);
+            this._selectEventLayer.drawRect(0, 0, this._width, this._height);
             this._selectEventLayer.interactive = true;
+
+
+
+            var updateId;
 
             this._selectEventLayer
                 .on('mousedown', this._onDragStart, this)
@@ -39,12 +64,25 @@ define(
             this._selectDraw = new Selection();
             this._stage.addChild(this._selectDraw);
 
-            this._stage.addChild(new Tools(this));
+            this._Tools = new Tools(this)
+            this._stage.addChild(this._Tools);
 
             this._animate();
 
             var allSelectedByPlanet = null;
             var engine = this;
+
+            Engine.prototype.onresize = function(w, h) {
+                this._width = w - 20;
+                this._height = h - 20;
+                this._renderer.resize(this._width, this._height);
+                this._background.resize(this._width, this._height);
+                this._resizeObj();
+                this._Tools._resize();
+            };
+
+
+
             this._dragging = false;
             this._planetHandlers = {
                 down: function(){
@@ -96,6 +134,38 @@ define(
             }
         }
 
+
+        Engine.prototype._calcDim = function()
+        {
+            this.ratio = this._mapWidth / this._mapHeight;
+            if (this._width > this._height)
+            {
+                this.scaleheight = this._height / this._mapHeight;
+                this.scalewidth = (this._height * this.ratio) / this._mapWidth;
+                this.scalerad = this.scaleheight;
+                this.scaleShip =  this._height / this.shipCountAtSide;
+
+            }else{
+                this.scalewidth = this._width / this._mapWidth;
+                this.scaleheight =  (this._width / this.ratio) / this._mapHeight;
+                this.scalerad = this.scalewidth;
+                this.scaleShip =  this._width  / this.shipCountAtSide;
+            }
+            this.xshift =  (this._width - this._mapWidth * this.scalewidth) / 2;
+            this.yshift = (this._height - this._mapHeight * this.scaleheight) / 2;
+        }
+
+        Engine.prototype._resizeObj = function()
+        {
+            this._calcDim();
+            if (Object.keys(this._planets).length > 0) {
+                Object.keys(this._planets).forEach(function(key) {
+                    pl = this._planets[key];
+                    pl.move(pl.origX * this.scalewidth + this.xshift, pl.origY * this.scaleheight + this.yshift, pl.origRad * this.scalerad);
+                }, this);
+            }
+        }
+
         Engine.prototype._animate = function(){
             this._background.update();
             this._renderer.render(this._stage);
@@ -130,17 +200,32 @@ define(
                     this._planetsOwned.has(this._planets[key]);
             }, this);
         };
-
+        Engine.prototype.ownerColor = function(owner) { return this.colorScheme[owner];}
         Engine.prototype.update = function(msg, playerId) {
-            function ownerColor(owner) { return owner & 0xFFFFFF; }
+
+            this._mapHeight = msg.height;
+            this._mapWidth = msg.width;
+            this.ratio = msg.width / msg.height;
+            this._calcDim();
+
             this._planetsOwned.clear();
             msg.planets.forEach(function(pl){
                 if (!this._planets[pl.id]) {
-                    this._planets[pl.id] = new Planet(pl.id, pl.x, pl.y, pl.radius, ownerColor(pl.owner),
-                        pl.ships_sum, this._planetHandlers);
+                    this._planets[pl.id] = new Planet(
+                        pl.id,
+                        pl.x * this.scalewidth + this.xshift,
+                        pl.y * this.scaleheight + this.yshift,
+                        pl.radius * this.scalerad,
+                        this.ownerColor(pl.owner),
+                        pl.ships_sum,
+                        this._planetHandlers,
+                        pl.x,
+                        pl.y,
+                        pl.radius
+                    );
                     this._objectsLayer.addChild(this._planets[pl.id]);
                 } else
-                    this._planets[pl.id].update(ownerColor(pl.owner), pl.ships_sum);
+                    this._planets[pl.id].update(this.ownerColor(pl.owner), pl.ships_sum);
                 if (pl.owner == playerId)
                     this._planetsOwned.add(this._planets[pl.id]);
             }, this);
@@ -154,9 +239,21 @@ define(
 
             msg.ships.forEach(function(sh, i){
                 if (i < this._ships.length)
-                    this._ships[i].update(sh.x, sh.y, sh.angle, ownerColor(sh.owner));
+                    this._ships[i].update(
+                        sh.x  * this.scalewidth + this.xshift,
+                        sh.y * this.scaleheight + this.yshift,
+                        sh.angle,
+                        this.ownerColor(sh.owner),
+                        this.scaleShip
+                    );
                 else {
-                    this._ships.push(new Ship(sh.x, sh.y, sh.angle, ownerColor(sh.owner)));
+                    this._ships.push(new Ship(
+                        sh.x  * this.scalewidth + this.xshift,
+                        sh.y * this.scaleheight + this.yshift,
+                        sh.angle,
+                        this.ownerColor(sh.owner),
+                        this.scaleShip
+                    ));
                     this._objectsLayer.addChild(this._ships[this._ships.length - 1]);
                 }
             }, this);
--- web/assets/js/engine/tools.js
+++ web/assets/js/engine/tools.js
@@ -3,16 +3,21 @@ define(
     function(PIXI, FullScreen) {
         function Tools(engine) {
             PIXI.Container.call(this);
-
-            var fullScreen = new FullScreen(engine.view);
-            fullScreen.x = engine._renderer.width - fullScreen.width - 10;
-            fullScreen.y = engine._renderer.height - fullScreen.height - 10;
-            this.addChild(fullScreen);
+            this._engine = engine;
+            this._fullScreen = new FullScreen(engine.view);
+            this._resize();
+            this.addChild(this._fullScreen);
         }
 
         Tools.prototype = Object.create(PIXI.Container.prototype);
         Tools.prototype.constructor = Tools;
 
+        Tools.prototype._resize = function()
+        {
+            this._fullScreen.x =  this._engine._renderer.width - this._fullScreen.width - 20;
+            this._fullScreen.y =  this._engine._renderer.height - this._fullScreen.height - 20;
+            this._fullScreen._draw(1);
+        }
         return Tools;
     }
 );
